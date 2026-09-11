5d9e147381ac102c05ffca9592eddc6417518cd0
alextarigan <alextarigan48@gmail.com>
2026-09-12 02:40:03 +0700

    update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
-import Photobooth from './components/Photobooth.jsx' // Sesuaikan jalur foldernya jika berbeda
+import Photobooth from './components/Photobooth.jsx' // Sesuaikan path jika berbeda
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        {/* Halaman Utama Undangan */}
         <Route path="/" element={<App />} />
-        
-        {/* Halaman Photobooth */}
         <Route path="/photobooth" element={<Photobooth />} />
       </Routes>
     </BrowserRouter>
-  </StrictMode>,
+  </React.StrictMode>,
 )
